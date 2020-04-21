3442353d39957a2d9ae846853dcb515f3b759f50
tt2468 <tt2468@gmail.com>
2020-04-21 04:14:04 -0700

    Add first sample

---
+++ samples/sample_request.py
@@ -0,0 +1,17 @@
+import asyncio
+import obsws
+
+loop = asyncio.get_event_loop()
+ws = obsws.obsws(host='127.0.0.1', port=4444, password='MYSecurePassword', loop=loop) # Every possible argument has been passed, but none are required. See lib code for defaults.
+
+async def make_request():
+    await ws.connect() # Make the connection to OBS-Websocket
+    result = await ws.call('GetVersion') # We get the current OBS version. More request data is not required
+    print(result) # Print the raw json output of the GetVersion request
+    await asyncio.sleep(1)
+    data = {'source':'test_source', 'volume':0.5}
+    result = await ws.call('SetVolume', data) # Make a request with the given data
+    print(result)
+    await ws.disconnect() # Clean things up by disconnecting. Only really required in a few specific situations, but good practice if you are done making requests or listening to events.
+
+loop.run_until_complete(make_request())
