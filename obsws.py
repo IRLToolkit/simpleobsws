83289a8149ff33b6695f35a2bfd010e2c85f42f0
tt2468 <tt2468@gmail.com>
2020-04-21 02:49:15 -0700

    Main functionality, missing events

--- obsws.py
+++ obsws.py
@@ -7,85 +7,87 @@ import uuid
 import time
 import sys
 
-async def obsws_init_connection(uri, password=None):
-    global ws
-    ws = await websockets.connect(uri)
-    request_id = str(uuid.uuid1())
-    requestpayload = {'message-id':request_id, 'request-type':'GetAuthRequired'}
-    await ws.send(json.dumps(requestpayload))
-    result = json.loads(await ws.recv())
-    if result['status'] != 'ok':
-        raise Exception('OBSWS Error: {}'.format(result['error']))
-    if result['authRequired']:
-        if password == None:
-            raise Exception('A password is required but was not provided')
-        secret = base64.b64encode(hashlib.sha256((password + result['salt']).encode('utf-8')).digest())
-        auth = base64.b64encode(hashlib.sha256(secret + result['challenge'].encode('utf-8')).digest()).decode('utf-8')
-        auth_payload = {"request-type": "Authenticate", "message-id": str(uuid.uuid1()), "auth": auth}
-        await ws.send(json.dumps(auth_payload))
-        result = json.loads(await ws.recv())
-        if result['status'] != 'ok':
-            raise Exception('OBSWS Error: {}'.format(result['error']))
-    global ws_answers
-    ws_answers = {}
-    loop.create_task(obsws_recv_task())
-
-async def obsws_call(request_type, data=None, timeout=30):
-    global ws_answers
-    if type(data) != dict and data != None:
-        raise Exception('Input data must be valid dict object')
-    request_id = str(uuid.uuid1())
-    requestpayload = {'message-id':request_id}
-    requestpayload['request-type'] = request_type
-    if data != None:
-        for key in data.keys():
-            requestpayload[key] = data[key]
-    await ws.send(json.dumps(requestpayload))
-    waittimeout = time.time() + timeout
-    await asyncio.sleep(0.05)
-    while time.time() < waittimeout:
-        if request_id in ws_answers:
-            returndata = ws_answers.pop(request_id)
-            returndata.pop('message-id')
-            return returndata
-        await asyncio.sleep(0.1)
-    raise Exception('The request with type {} timed out after () seconds.'.format(request_type, timeout))
-    
-
-async def obsws_recv_task():
-    global ws_answers
-    try:
-        while ws.open:
-            message = ''
-            message = await ws.recv()
-            if not message:
-                continue
-            result = json.loads(message)
-            if 'update-type' in result:
-                update_type = result['update-type']
-                del result['update-type']
-                await obsws_handle_event(update_type, result)
-            elif 'message-id' in result:
-                ws_answers[result['message-id']] = result
-            else:
-                print('Unknown message: {}'.format(result))
-    except:
-        pass
-    print('OBSWS connection has been closed.')
-    raise KeyboardInterrupt
-
-
-async def obsws_handle_event(update_type, data=None):
+class ConnectionFailure(Exception):
     pass
-    print(update_type)
-    print(data)
+class MessageTimeout(Exception):
+    pass
+class MessageFormatError(Exception):
+    pass
 
-async def test_task():
-    await asyncio.sleep(2)
-    print(await obsws_call('ListSceneCollections'))
+class obsws:
+    def __init__(self, host='localhost', port=4444, password=None, loop: asyncio.BaseEventLoop=None):
+        self.ws = None
+        self.answers = {}
+        self.loop = loop or asyncio.get_event_loop()
+        self.recv_task = None
+        
+        self.host = host
+        self.port = port
+        self.password = password
+    
+    async def connect(self)
+        if self.ws.open:
+            raise ConnectionFailure('Server is already connected.')
+        self.ws = await websockets.connect('ws://{}:{}'.format(self.host, self.port))
+        requestpayload = {'message-id':'1', 'request-type':'GetAuthRequired'}
+        await self.ws.send(json.dumps(requestpayload))
+        getauthresult = json.loads(await self.ws.recv())
+        if getauthresult['status'] != 'ok':
+            raise ConnectionFailure('Server returned error to GetAuthRequired request: {}'.format(getauthresult['error']))
+        if getauthresult['authRequired']:
+            if self.password == None:
+                raise ConnectionFailure('A password is required by the server but was not provided')
+            secret = base64.b64encode(hashlib.sha256((self.password + getauthresult['salt']).encode('utf-8')).digest())
+            auth = base64.b64encode(hashlib.sha256(secret + getauthresult['challenge'].encode('utf-8')).digest()).decode('utf-8')
+            auth_payload = {"request-type": "Authenticate", "message-id": '2', "auth": auth}
+            await self.ws.send(json.dumps(auth_payload))
+            authresult = json.loads(await self.ws.recv())
+            if authresult['status'] != 'ok':
+                raise ConnectionFailure('Server returned error to Authenticate request: {}'.format(authresult['error']))
+        self.recv_task = self.loop.create_task(self._ws_recv_task())
+    
+    async def disconnect(self):
+        await self.ws.close()
+        self.recv_task.cancel()
+        self.recv_task = None
+    
+    async def call(self, request_type, data=None, timeout=15):
+        if type(data) != dict and data != None:
+            raise MessageFormatError('Input data must be valid dict object')
+        request_id = str(uuid.uuid1())
+        requestpayload = {'message-id':request_id, 'request-type':request_type}
+        if data != None:
+            for key in data.keys():
+                if key == 'message-id':
+                    continue
+                requestpayload[key] = data[key]
+        await self.ws.send(json.dumps(requestpayload))
+        waittimeout = time.time() + timeout
+        await asyncio.sleep(0.05) # This acts to halve request time on LAN connections for most requests.
+        while time.time() < waittimeout:
+            if request_id in self.answers:
+                returndata = ws.answers.pop(request_id)
+                returndata.pop('message-id')
+                return returndata
+            await asyncio.sleep(0.1) # Default timeout period. Change this to adjust the polling period for new messages.
+        raise MessageTimeout('The request with type {} timed out after {} seconds.'.format(request_type, timeout))
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(obsws_init_connection('ws://127.0.0.1:4444', 'ttpass'))
-    loop.create_task(test_task())
-    loop.run_forever()
+    async def _ws_recv_task():
+        while self.ws.open:
+            message = ''
+            try:
+                message = await ws.recv()
+                if not message:
+                    continue
+                result = json.loads(message)
+                if 'update-type' in result:
+                    pass
+#                    update_type = result['update-type']
+#                    del result['update-type']
+#                    await obsws_handle_event(update_type, result)
+                elif 'message-id' in result:
+                    self.answers[result['message-id']] = result
+                else:
+                    print('Unknown message: {}'.format(result))
+            except (websockets.exceptions.ConnectionClosed, websockets.exceptions.ConnectionClosedError, websockets.exceptions.ConnectionClosedOK):
+                break
