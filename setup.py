92b42c17c6b9c2436ec6a3426fcc0f70aa084e8a
tt2468 <tt2468@gmail.com>
2020-04-21 05:07:25 -0700

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = open('requirements.txt', 'rt').readlines()
 
 setuptools.setup(
     name="simpleobsws", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="tt2468",
     author_email="tt2468@gmail.com",
     description="A simple obs-websocket library in async Python for people who just want JSON output.",
