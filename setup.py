82a3da24ac4583d61028aaa8f8a4267299bfa4c7
tt2468 <tt2468@gmail.com>
2020-04-21 05:02:29 -0700

    Forgot to add the package

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/IRLToolkit/simpleobsws",
-    packages=setuptools.find_packages(),
+    packages=['simpleobsws'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
